f55aedefd300d9497d1d7c06cf4d02c06905e37e
EthanMcMullen <113043142+EthanMcMullen@users.noreply.github.com>
2024-04-22 11:10:39 -0400

    db.js, mongo set up

---
+++ EMR/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://emcmullen:MHpBLwtICxxCSri3@mevn-sports-application.n8va8gg.mongodb.net/'
+//const connectionString = 'mongodb+srv://EthanMcMullen:oE9PpZGE4x7rl9F7@cluster0.eyajqey.mongodb.net';
+const connectToMongoDB = async (uri) => {
+    try {
+      await mongoose.connect(uri);
+      console.log('Connected to MongoDB Atlas');
+    } catch (err) {
+      console.error('Error connecting to MongoDB Atlas:', err);
+    }
+  };
+
+  connectToMongoDB(connectionString)
+  
+module.exports = connectToMongoDB;
