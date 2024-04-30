09649f6c581769cd4bf968be7261d35ff02a2f27
EthanMcMullen <113043142+EthanMcMullen@users.noreply.github.com>
2024-04-30 10:35:31 -0400

    profileView, Going to switch to a branch now

--- emr-frontend/src/App.vue
+++ emr-frontend/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/profile">Profile</RouterLink>
       </nav>
     </div>
   </header>
