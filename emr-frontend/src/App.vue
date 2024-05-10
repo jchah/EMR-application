0c32a973b9f4281e9a5b660b5de70c58b823e42e
Jad <98987031+jchah@users.noreply.github.com>
2024-05-10 11:08:49 -0400

    VUE WORKING

--- emr-frontend/src/App.vue
+++ emr-frontend/src/App.vue
@@ -1,34 +1,30 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link>
-  </nav>
-  <router-view />
-</template>
-<script>
-
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-nav {
-  padding: 30px;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- emr-frontend/src/main.js
+++ emr-frontend/src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-store.dispatch('rehydrateAuthenticationState');
-
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).mount('#app')
