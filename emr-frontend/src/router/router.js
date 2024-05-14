1fabb3af119fd16595b80828c54dcd31606dd099
EthanMcMullen <emcmullen@bayviewglen.ca>
2024-05-13 21:30:35 -0400

    Navbar + Patient Search

--- emr-frontend/src/router/router.js
+++ emr-frontend/src/router/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Dashboard from '../views/Dashboard.vue';
-// import Patients from './views/Patients.vue';
+import Patients from '../views/Patients.vue';
 // import Appointments from './views/Appointments.vue';
 import Settings from '../views/Settings.vue';
 
 const routes = [
     { path: '/', component: Dashboard },
-    // { path: '/patients', component: Patients },
+    { path: '/patients', component: Patients },
     // { path: '/appointments', component: Appointments },
     { path: '/settings', component: Settings },
 ];
