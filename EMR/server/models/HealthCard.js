e5b34aa4b42cd7ab2529deff898e66d352d6a404
Jad <98987031+jchah@users.noreply.github.com>
2024-05-28 10:57:55 -0400

    match

--- EMR/server/models/HealthCard.js
+++ EMR/server/models/HealthCard.js
@@ -13,6 +13,7 @@ const healthCardSchema = new Schema({
     dateOfBirth: {
         type: String,
         required: true,
+        match: [/^\d{4}-\d{2}-\d{2}$/, 'Please fill a valid date in YYYY-MM-DD format'],
     },
     sex: {
         type: String,
