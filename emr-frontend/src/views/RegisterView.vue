c54ebe8765d7b6af5858ebbd86433d72ebca63da
MCarnegie <mcarnegie@bayviewglen.ca>
2024-05-30 10:34:29 -0400

    added functionality to dashboard buttons and made the resigter view without functionality for owen

--- emr-frontend/src/views/RegisterView.vue
+++ emr-frontend/src/views/RegisterView.vue
@@ -1,3 +1,27 @@
 <template>
-<h1 class="title">registration page</h1>
-</template>
+    <div class="section">
+      <div class="container is-centered">
+        <div class="columns is-centered">
+          <div class="column is-one-third">
+            <div class="card">
+              <div class="card-content">
+                <h1 class="title">Register New Account</h1>
+                <form>
+                  <div class="field">
+                    <label class="label" for="email">Email:</label>
+                    <input class="input" type="email" id="email" required>
+                  </div>
+                  <div class="field">
+                    <label class="label" for="password">Password:</label>
+                    <input class="input" type="text" id="password" required>
+                  </div>
+                  <button type="submit" class="button">Register</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
