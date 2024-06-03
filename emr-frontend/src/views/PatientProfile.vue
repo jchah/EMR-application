b9dfdeb33e78c2d3d86ae9157dce6a51c87f31a0
EthanMcMullen <emcmullen@bayviewglen.ca>
2024-06-03 17:32:04 -0400

    Sorry fixed it

--- emr-frontend/src/views/PatientProfile.vue
+++ emr-frontend/src/views/PatientProfile.vue
@@ -34,54 +34,8 @@
           <div class="column">
             <p class="title has-text-centered">Appointments</p>
           </div>
-          <div class="column is-one-quarter">
-            <p class="title has-text-centered">Book Apointment For Patient?</p>
-
-            <h1 class="title">Make an Appointment</h1>
-              <div class="box">
-                <form @submit.prevent="makeAppointment(date)">
-                  <div class="field">
-                    <label class="label">Selected Date: {{ formatDate(date) }}</label>
-                  </div>
-                    <label class="label">Patient Name</label>
-                      <div class="control">
-                        <input class="input" type="text" v-model="inSearchBar" required>
-                          <div class="control">
-                            <div class="dropdown is-active" v-if="showOptions()">
-                              <div class="dropdown-menu" style="position: absolute;">
-                                <div class="dropdown-content">
-                                  <a v-for="option in searchOptions"  @click="selectOption(option)" class="dropdown-item">{{ option }}</a>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-
-                        <div class="field">
-                          <div class="control beside">
-                            <label class="label">Start Time</label>
-                            <DatePicker mode="time" v-model="info.startTime"/>
-                              <label class="label">End Time</label>
-                            <DatePicker mode="time" v-model="info.endTime"/>
-                          </div>
-                        </div>
-                        <div class="field">
-                          <label class="label">Notes</label>
-                            <div class="control">
-                              <input class="input" type="text" v-model="info.notes">
-                            </div>
-                        </div>
-                          
-                      <div class="field is-grouped">
-                      <div class="control">
-                          <button class="button is-primary" type="submit">Submit</button>
-                      </div>
-                      </div>
-                  </form>
-              </div>
-                         
-          </div>
-        </div>
+          
+      </div>
       
   </template>
      
