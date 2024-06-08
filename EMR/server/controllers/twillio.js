404ad9e6c9743735c5053565a1423f1c28049109
Omer Hussein <ohussein@bayviewglen.ca>
2024-06-08 02:14:13 -0400

    fixed sms

--- EMR/server/controllers/twillio.js
+++ EMR/server/controllers/twillio.js
@@ -1,6 +1,10 @@
 const twilio = require('twilio');
-const accountSid = 'ACd1a80cae6b123c47872ce2e5bacb3958';
-const authToken = '9393852fbda67a293af0f9904836dae4';
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 
 const client = new twilio(accountSid, authToken);
 
