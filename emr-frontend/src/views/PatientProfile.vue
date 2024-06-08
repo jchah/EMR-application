0f5da68cefde63cd483dc9514363c3f285ec1d49
Owen Chen <73706094+Spxxk@users.noreply.github.com>
2024-06-08 14:07:00 -0400

    done

--- emr-frontend/src/views/PatientProfile.vue
+++ emr-frontend/src/views/PatientProfile.vue
@@ -6,226 +6,234 @@
     </div>
   </section>
 
-  <div class="columns has-background-info-light">
-    <div class="column has-text-centered">
-      <p class="title has-text-centered">Patient Info</p>
+  <div v-if="patient">
+    <div class="card my-4">
+      <div class="card-content">
+        <div class="columns is-mobile">
+          <div class="column has-text-centered">
+            <p class="title has-text-centered">Patient Info</p>
+          </div>
+        </div>
+        <div class="columns has-text-centered info">
+          <div class="column">
+            <div class="columns">
+              <div class="column">
+                <p class="subtitle">First Name : {{ patient.firstName }}</p>
+                <p class="subtitle">Last Name : {{ patient.lastName }}</p>
+              </div>
+              <div class="column">
+                <p class="subtitle">Sex : {{ patient.sex }}</p>
+                <p class="subtitle">Health Card Number : {{ patient.cardNumber }}</p>
+              </div>
+              <div class="column">
+                <p class="subtitle">DOB : {{ patient.dateOfBirth }}</p>
+                <p class="subtitle">Address : {{ patient.address }}</p>
+              </div>
+              <div class="column">
+                <p class="subtitle">Phone : {{ patient.contact.phone }}</p>
+                <p class="subtitle">Email : {{ patient.contact.email }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns has-text-centered">
+          <div class="column">
+            <button class="button is-info is-size-5 mt-3" @click="openContactOverlay(true)">Change Contact Preference?</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card my-4">
+      <div class="card-content">
+        <div class="columns is-mobile">
+          <div class="column has-text-centered">
+            <p class="title has-text-centered">Conditions</p>
+          </div>
+        </div>
+        <div class="columns info">
+          <div class="column">
+            <table class="table is-fullwidth">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th class="has-text-centered is-size-5">Condition Name</th>
+                  <th class="has-text-centered is-size-5">Treatment Name</th>
+                  <th class="has-text-centered is-size-5">Frequency</th>
+                  <th class="has-text-centered is-size-5">Start Date</th>
+                  <th class="has-text-centered is-size-5">End Date</th>
+                  <th class="has-text-centered is-size-5">Route</th>
+                  <th class="has-text-centered is-size-5">Dosage</th>
+                  <th class="has-text-centered is-size-5">Prescribing Physician</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="treatment in treatments" :key="treatment._id">
+                  <td><button class="button is-danger is-small" @click="clearTreatment(treatment)">X</button></td>
+                  <td class="has-text-centered is-bold">{{ treatment.condition }}</td>
+                  <td class="has-text-centered">{{ treatment.name }}</td>
+                  <td class="has-text-centered">{{ treatment.frequency }}</td>
+                  <td class="has-text-centered">{{ new Date(treatment.startDate).toLocaleDateString() }}</td>
+                  <td class="has-text-centered">{{ new Date(treatment.endDate).toLocaleDateString() }}</td>
+                  <td class="has-text-centered">{{ treatment.route }}</td>
+                  <td class="has-text-centered">{{ treatment.dosage }}</td>
+                  <td class="has-text-centered">{{ treatment.prescribingPhysician }}</td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+            <div v-if="token" class="has-text-centered">
+              <button class="button is-danger has-text-centered is-size-5 mt-3" @click="openTreatmentForm(true)">Add Treatment?</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card my-4">
+      <div class="card-content">
+        <div class="columns is-mobile">
+          <div class="column has-text-centered">
+            <p class="title has-text-centered">Appointments</p>
+          </div>
+        </div>
+        <div class="columns info">
+          <div class="column">
+            <p class="title has-text-centered"></p>
+            <table class="table is-fullwidth">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th class="has-text-centered is-size-5">Notes</th>
+                  <th class="has-text-centered is-size-5">Date</th>
+                  <th class="has-text-centered is-size-5">Start Time</th>
+                  <th class="has-text-centered is-size-5">End Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="appointment in appointments" :key="appointment._id">
+                  <td><button class="button is-danger is-small" @click="deleteAppointment(appointment._id)">X</button></td>
+                  <td class="has-text-centered">{{ appointment.notes }}</td>
+                  <td class="has-text-centered">{{ appointment.date }}</td>
+                  <td class="has-text-centered">{{ appointment.startTime }}</td>
+                  <td class="has-text-centered">{{ appointment.endTime }}</td>
+                </tr>
+              </tbody>
+            </table>
+            <div class="has-text-centered">
+              <button class="button is-warning is-size-5 mt-3" @click="sendToAppointmentPage()">Create Appointment?</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
-  <div v-if="patient">
-    <div class="columns has-background-info-light has-text-centered info">
-      <div class="column">
+  <div class="overlay" v-if="contactOpen">
+    <div class="box">
+      <form @submit.prevent="addTreatment()">
+        <div class="has-text-centered">
+          <p class="title">Current Preference : {{ patient.contactPreference }}</p>
+          <p class="title">___________________________</p>
+          <br />
+          <p class="title">Select New Preference:</p>
+        </div>
+        <div class="columns has-text-centered">
+          <div class="column">
+            <button class="button is-info is-size-3" @click="selectPreference('email')">Email</button>
+          </div>
+          <div class="column">
+            <button class="button is-info is-size-3" @click="selectPreference('sms')">SMS</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div class="overlay" v-if="windowOpen">
+    <div class="box">
+      <form @submit.prevent="addTreatment()">
+        <br />
+        <p class="title has-text-centered">Add New Treatment</p>
         <div class="columns">
           <div class="column">
-            <p class="subtitle">First Name : {{ patient.firstName }}</p>
-            <p class="subtitle">Last Name : {{ patient.lastName }}</p>
+            <div class="field">
+              <label class="label">Condition Name</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newTreatment.condition" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Treatment Name</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newTreatment.name" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Dosage</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newTreatment.dosage" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Frequency</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newTreatment.frequency" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Route</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newTreatment.route" />
+              </div>
+            </div>
           </div>
+
           <div class="column">
-            <p class="subtitle">Sex : {{ patient.sex }}</p>
-            <p class="subtitle">Health Card Number : {{ patient.cardNumber }}</p>
-          </div>
-          <div class="column">
-            <p class="subtitle">DOB : {{ patient.dateOfBirth }}</p>
-            <p class="subtitle">Address : {{ patient.address }}</p>
-          </div>
-          <div class="column">
-            <p class="subtitle">Phone : {{ patient.contact.phone }}</p>
-            <p class="subtitle">Email : {{ patient.contact.email }}</p>
+            <div class="field">
+              <label class="label">Start Date</label>
+              <div class="control">
+                <input class="input" type="date" v-model="newTreatment.startDate" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">End Date</label>
+              <div class="control">
+                <input class="input" type="date" v-model="newTreatment.endDate" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Prescribing Physician</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newTreatment.prescribingPhysician" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Notes</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newTreatment.notes" />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-
-    <div class="columns has-text-centered has-background-info-light">
-      <div class="column">
-        <button class="button is-info is-size-4" @click="openContactOverlay(true)">Change Contact Preference?</button>
-      </div>
-    </div>
-
-    <div class="columns has-background-danger-light">
-      <div class="column has-text-centered">
-        <p class="title has-text-centered">Conditions</p>
-      </div>
-    </div>
-
-    <div class="columns has-background-danger-light info">
-      <div class="column">
-        <table class="table is-fullwidth has-background-danger-light">
-          <thead>
-            <tr>
-              <th></th>
-              <th class="has-text-centered is-size-4">Condition Name</th>
-              <th class="has-text-centered is-size-4">Treatment Name</th>
-              <th class="has-text-centered is-size-4">Frequency</th>
-              <th class="has-text-centered is-size-4">Start Date</th>
-              <th class="has-text-centered is-size-4">End Date</th>
-              <th class="has-text-centered is-size-4">Route</th>
-              <th class="has-text-centered is-size-4">Dosage</th>
-              <th class="has-text-centered is-size-4">Prescribing Physician</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="treatment in treatments" :key="treatment._id">
-              <td><button class="button is-danger" @click="clearTreatment(treatment)">X</button></td>
-              <td class="has-text-centered is-bold">{{ treatment.condition }}</td>
-              <td class="has-text-centered">{{ treatment.name }}</td>
-              <td class="has-text-centered">{{ treatment.frequency }}</td>
-              <td class="has-text-centered">{{ new Date(treatment.startDate).toLocaleDateString() }}</td>
-              <td class="has-text-centered">{{ new Date(treatment.endDate).toLocaleDateString() }}</td>
-              <td class="has-text-centered">{{ treatment.route }}</td>
-              <td class="has-text-centered">{{ treatment.dosage }}</td>
-              <td class="has-text-centered">{{ treatment.prescribingPhysician }}</td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        <div v-if="token" class="has-text-centered">
-          <button class="button is-danger has-text-centered" @click="openTreatmentForm(true)">Add Treatment?</button>
+        <div class="columns has-text-centered">
+          <div class="column">
+            <div class="field is-grouped">
+              <div class="control">
+                <button class="button is-primary" type="submit">Submit</button>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <button class="button is-info" @click="clearTreatmentForm()">Clear Form</button>
+          </div>
+          <div class="column">
+            <button class="button is-danger" @click="openTreatmentForm(false)">Cancel</button>
+          </div>
         </div>
-      </div>
-    </div>
-
-    <div class="columns has-background-warning-light">
-      <div class="column has-text-centered">
-        <p class="title has-text-centered">Appointments</p>
-      </div>
-    </div>
-
-    <div class="columns has-background-warning-light info">
-      <div class="column">
-        <p class="title has-text-centered"></p>
-        <table class="table is-fullwidth has-background-warning-light">
-          <thead>
-            <tr>
-              <th></th>
-              <th class="has-text-centered is-size-4">Notes</th>
-              <th class="has-text-centered is-size-4">Date</th>
-              <th class="has-text-centered is-size-4">Start Time</th>
-              <th class="has-text-centered is-size-4">End Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="appointment in appointments" :key="appointment._id">
-              <td><button class="button is-danger" @click="deleteAppointment(appointment._id)">X</button></td>
-              <td class="has-text-centered">{{ appointment.notes }}</td>
-              <td class="has-text-centered">{{ appointment.date }}</td>
-              <td class="has-text-centered">{{ appointment.startTime }}</td>
-              <td class="has-text-centered">{{ appointment.endTime }}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div  class="has-text-centered">
-          <button class="button is-warning" @click="sendToAppointmentPage()">Create Appointment?</button>
-        </div>
-      </div>
-    </div>
-
-    <div class="overlay" v-if="contactOpen">
-      <div class="box">
-        <form @submit.prevent="addTreatment()">
-          <div class="has-text-centered">
-            <p class="title">Current Preference : {{ patient.contactPreference }}</p>
-            <p class="title">___________________________</p>
-            <br />
-            <p class="title">Select New Preference:</p>
-          </div>
-          <div class="columns has-text-centered">
-            <div class="column">
-              <button class="button is-info is-size-3" @click="selectPreference('email')">Email</button>
-            </div>
-            <div class="column">
-              <button class="button is-info is-size-3" @click="selectPreference('sms')">SMS</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-
-    <div class="overlay" v-if="windowOpen">
-      <div class="box">
-        <form @submit.prevent="addTreatment()">
-          <br />
-          <p class="title has-text-centered">Add New Treatment</p>
-          <div class="columns">
-            <div class="column">
-              <div class="field">
-                <label class="label">Condition Name</label>
-                <div class="control">
-                  <input class="input" type="text" v-model="newTreatment.condition" />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Treatment Name</label>
-                <div class="control">
-                  <input class="input" type="text" v-model="newTreatment.name" />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Dosage</label>
-                <div class="control">
-                  <input class="input" type="text" v-model="newTreatment.dosage" />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Frequency</label>
-                <div class="control">
-                  <input class="input" type="text" v-model="newTreatment.frequency" />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Route</label>
-                <div class="control">
-                  <input class="input" type="text" v-model="newTreatment.route" />
-                </div>
-              </div>
-            </div>
-
-            <div class="column">
-              <div class="field">
-                <label class="label">Start Date</label>
-                <div class="control">
-                  <input class="input" type="date" v-model="newTreatment.startDate" />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">End Date</label>
-                <div class="control">
-                  <input class="input" type="date" v-model="newTreatment.endDate" />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Prescribing Physician</label>
-                <div class="control">
-                  <input class="input" type="text" v-model="newTreatment.prescribingPhysician" />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Notes</label>
-                <div class="control">
-                  <input class="input" type="text" v-model="newTreatment.notes" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="columns has-text-centered">
-            <div class="column">
-              <div class="field is-grouped">
-                <div class="control">
-                  <button class="button is-primary" type="submit">Submit</button>
-                </div>
-              </div>
-            </div>
-            <div class="column">
-              <button class="button is-info" @click="clearTreatmentForm()">Clear Form</button>
-            </div>
-            <div class="column">
-              <button class="button is-danger" @click="openTreatmentForm(false)">Cancel</button>
-            </div>
-          </div>
-        </form>
-        <br />
-      </div>
+      </form>
+      <br />
     </div>
   </div>
 </template>
@@ -406,5 +414,14 @@ export default {
 
 .is-align-items-center {
   align-items: center;
+}
+
+.card {
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+  border-radius: 6px;
+}
+
+.card-content {
+  padding: 1.5rem 2rem;
 }
 </style>
